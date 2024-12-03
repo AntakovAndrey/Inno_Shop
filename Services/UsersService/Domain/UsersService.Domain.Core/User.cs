3b0b37cc4867fa56c9b409505275dd7ad4f789ff
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-03 14:30:32 +0300

    users service user model added

---
+++ Services/UsersService/Domain/UsersService.Domain.Core/User.cs
@@ -0,0 +1,14 @@
+﻿using UsersService.Domain.Core.Enums;
+
+namespace UsersService.Domain.Core
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+        public UserStatus UserStatus { get; set; }
+    }
+}
