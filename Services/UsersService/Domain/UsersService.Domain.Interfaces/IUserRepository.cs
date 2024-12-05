237ea2216f0a50121a1a9b0e63ccd6fb6e443e79
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-05 11:09:51 +0300

    IUserRepository added to UsersService.Domain.Interfaces

---
+++ Services/UsersService/Domain/UsersService.Domain.Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using UsersService.Domain.Core;
+
+namespace UsersService.Domain.Interfaces
+{
+    public interface IUserRepository:IDisposable
+    {
+        User GetUser(int id);
+        IEnumerable<User> GetUsers();
+        void Create(User user);
+        void Update(User user);
+        void Delete(int id);
+        void Save();
+    }
+}
