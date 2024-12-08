b9746de19e7034bdf17bcd38b58dec53b00060b5
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-09 01:16:29 +0300

    sending email implemented

---
+++ Services/UsersService/Infrastructure/UsersService.Infrastructure.Services/EmailService.cs
@@ -0,0 +1,43 @@
+﻿using UsersService.Services.Interfaces;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace UsersService.Infrastructure.Services
+{
+    public class EmailService:IEmailService
+    {
+        private readonly string _host;
+        private readonly int _port;
+        private readonly string _username;
+        private readonly string _password;
+
+        public EmailService(string host,int port,string username,string password) 
+        {
+            _host = host;
+            _port = port;
+            _username = username;
+            _password = password;
+        }
+
+        public async Task SendMessage(string email, string subject, string body)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Inno shop", _username));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = body
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(_host, _port, true);
+                await client.AuthenticateAsync(_username, _password);
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
