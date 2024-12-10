b1bf74425e77439e7c31c1cd051ed4b519effe56
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-11 01:58:26 +0300

    ConfirmationCode DBSet added

--- Services/UsersService/Infrastructure/UsersService.Infrastructure.Data/UsersServiceDBContext.cs
+++ Services/UsersService/Infrastructure/UsersService.Infrastructure.Data/UsersServiceDBContext.cs
@@ -7,6 +7,7 @@ namespace UsersService.Infrastructure.Data
     public class UsersServiceDBContext:DbContext
     {
         public DbSet<User> Users{ get; set; }
+        public DbSet<UserConfirmationCode> ConfirmationCodes { get; set; }
 
         public UsersServiceDBContext(DbContextOptions<UsersServiceDBContext> options) : base(options)
         {
