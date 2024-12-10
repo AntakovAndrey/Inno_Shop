cdc2ac31689e0aaf09177adbc1c33b0c12844185
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-11 01:46:08 +0300

    user confirmation code entity added

---
+++ Services/UsersService/Domain/UsersService.Domain.Core/UserConfirmationCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersService.Domain.Core
+{
+    public class UserConfirmationCode
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
