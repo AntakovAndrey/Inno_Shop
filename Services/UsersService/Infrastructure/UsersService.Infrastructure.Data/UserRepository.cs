1bd7cf8f80ade6e131f331b5af49997981386b7e
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-11 01:57:01 +0300

    User reopsitory refactor

--- Services/UsersService/Infrastructure/UsersService.Infrastructure.Data/UserRepository.cs
+++ Services/UsersService/Infrastructure/UsersService.Infrastructure.Data/UserRepository.cs
@@ -6,30 +6,37 @@ namespace UsersService.Infrastructure.Data
     public class UserRepository:IUserRepository
     {
         private readonly UsersServiceDBContext _dbContext;
+
         public UserRepository(UsersServiceDBContext dBContext)
         {
-            _dbContext=dBContext;
+            _dbContext = dBContext;
         }
+
         public User GetUser(int id)
         {
             return _dbContext.Users.Find(id);
         }
+
         public IEnumerable<User> GetUsers()
         {
             return _dbContext.Users.ToList();
         }
+
         public void Create(User user)
         {
             _dbContext.Add(user);
         }
+
         public void Update(User user)
         {
             _dbContext.Update(user);
         }
+
         public void Delete(int id)
         {
             _dbContext.Users.Remove(GetUser(id));
         }
+
         public void Save()
         {
             _dbContext.SaveChanges();
