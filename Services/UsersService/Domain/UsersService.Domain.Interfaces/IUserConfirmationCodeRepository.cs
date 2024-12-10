3e34cafbc3417a6350450a8e23de5cf3d98f62a8
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-11 01:52:10 +0300

    IUserConfirmationCode Repository added

---
+++ Services/UsersService/Domain/UsersService.Domain.Interfaces/IUserConfirmationCodeRepository.cs
@@ -0,0 +1,14 @@
+﻿using UsersService.Domain.Core;
+
+namespace UsersService.Domain.Interfaces
+{
+    public interface IUserConfirmationCodeRepository
+    {
+        UserConfirmationCode GetConfirmationCode(int id);
+        UserConfirmationCode GetConfirmationCode(User user);
+        void Create(UserConfirmationCode userConfirmationCode);
+        void Update(UserConfirmationCode userConfirmationCode);
+        void Delete(int id);
+        void Save();
+    }
+}
