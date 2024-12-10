1ea0ae21f14a9e7d9befec73ef9c731bc0f334bb
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-11 01:57:29 +0300

    UserConfirmationCodeRepository implemented

---
+++ Services/UsersService/Infrastructure/UsersService.Infrastructure.Data/UserConfirmationCodeRepository.cs
@@ -0,0 +1,53 @@
+﻿using UsersService.Domain.Core;
+using UsersService.Domain.Interfaces;
+
+namespace UsersService.Infrastructure.Data
+{
+    public class UserConfirmationCodeRepository : IUserConfirmationCodeRepository
+    {
+        private readonly UsersServiceDBContext _dbContext;
+
+        public UserConfirmationCodeRepository(UsersServiceDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Create(UserConfirmationCode userConfirmationCode)
+        {
+            _dbContext.ConfirmationCodes.Add(userConfirmationCode);
+        }
+
+        public void Delete(int id)
+        {
+            var confirmationCode= _dbContext.ConfirmationCodes.First(x=>x.Id==id);
+            _dbContext.ConfirmationCodes.Remove(confirmationCode);
+        }
+
+        public UserConfirmationCode GetConfirmationCode(int id)
+        {
+            return _dbContext.ConfirmationCodes.First(x=>x.Id == id);
+        }
+
+        public UserConfirmationCode? GetConfirmationCode(User user)
+        {
+            try
+            {
+                return _dbContext.ConfirmationCodes.First(x => x.User == user);
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public void Update(UserConfirmationCode userConfirmationCode)
+        {
+            _dbContext.ConfirmationCodes.Update(userConfirmationCode);
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
