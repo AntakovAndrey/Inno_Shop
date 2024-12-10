bc270c29ae28d6e537ade93397b304645c48b871
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-11 01:42:38 +0300

    IUserService

---
+++ Services/UsersService/Services/UsersService.Services.Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using UsersService.Domain.Core;
+
+namespace UsersService.Services.Interfaces
+{
+    public interface IUserService
+    {
+        public string CreateConfirmationCode(User user);
+        public Task<String> GenerateTokenAsync(User user);
+        public bool ConfirmEmail(string email, string confirmationCode);
+    }
+}
