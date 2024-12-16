96bffbf4491bc3ba08051832f2d589d3f54d512c
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-16 03:44:58 +0300

    auth options added

--- Services/UsersService/API/Program.cs
+++ Services/UsersService/API/Program.cs
@@ -9,7 +9,43 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<UsersServiceDBContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IUserConfirmationCodeRepository, UserConfirmationCodeRepository>();
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddSingleton<IEmailService, EmailService>(x => 
+    new EmailService(
+        host: builder.Configuration["EmailSender:Host"],
+        port: Convert.ToInt32(builder.Configuration["EmailSender:Port"]),
+        username: builder.Configuration["EmailSender:Username"],
+        password: builder.Configuration["EmailSender:Password"]
+    ));
+builder.Services.AddSingleton<AuthOptions>(x =>
+    new AuthOptions
+    {
+        Audience = builder.Configuration["Auth:Audience"],
+        Expiration = TimeSpan.Parse(builder.Configuration["Auth:Expiration"]),
+        Issuer = builder.Configuration["Auth:Issuer"],
+        Secret = builder.Configuration["Auth:Secret"]
+    });
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Auth:Issuer"],
+            ValidAudience = builder.Configuration["Auth:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey( Encoding.UTF8.GetBytes( builder.Configuration["Auth:Secret"]) )
+        };
+    });
 
 builder.Services.AddControllers();
 
