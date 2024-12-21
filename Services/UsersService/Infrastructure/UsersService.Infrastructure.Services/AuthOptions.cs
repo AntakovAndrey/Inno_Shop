31e5f90d5ae26a261f002f652ed5220591566fe9
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-21 23:15:14 +0300

    auth optins added

---
+++ Services/UsersService/Infrastructure/UsersService.Infrastructure.Services/AuthOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace UsersService.Infrastructure.Services;
+
+public class AuthOptions
+{
+    public string Secret { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public TimeSpan Expiration { get; set; }
+}
