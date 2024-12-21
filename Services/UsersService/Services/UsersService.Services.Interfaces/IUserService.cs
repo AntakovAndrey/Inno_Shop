90a1b8246527a13bc56813e936a129ac6406fdad
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-21 23:17:38 +0300

    IUserService refactor

--- Services/UsersService/Services/UsersService.Services.Interfaces/IUserService.cs
+++ Services/UsersService/Services/UsersService.Services.Interfaces/IUserService.cs
@@ -4,8 +4,11 @@ namespace UsersService.Services.Interfaces
 {
     public interface IUserService
     {
+        public string CreateResetPasswordCode(User user);
         public string CreateConfirmationCode(User user);
         public Task<String> GenerateTokenAsync(User user);
+        public User Authenticate(string email, string password);
         public bool ConfirmEmail(string email, string confirmationCode);
+        public Task ResetPassword(User user, string newPassword,string resetPasswordCode);
     }
 }
