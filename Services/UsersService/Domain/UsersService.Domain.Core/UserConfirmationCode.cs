a414028de3075682f826aad56453ad6167c53175
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-21 23:01:58 +0300

    Users confirmation code added

--- Services/UsersService/Domain/UsersService.Domain.Core/UserConfirmationCode.cs
+++ Services/UsersService/Domain/UsersService.Domain.Core/UserConfirmationCode.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UsersService.Domain.Core
+﻿namespace UsersService.Domain.Core
 {
     public class UserConfirmationCode
     {
