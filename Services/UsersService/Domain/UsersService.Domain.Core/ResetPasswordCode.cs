6c3d32d207b2ad9230403829ba724e7559f4f7dc
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-21 23:03:38 +0300

    ResetPasswordCode added

---
+++ Services/UsersService/Domain/UsersService.Domain.Core/ResetPasswordCode.cs
@@ -0,0 +1,10 @@
+﻿namespace UsersService.Domain.Core
+{
+    public class ResetPasswordCode
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
