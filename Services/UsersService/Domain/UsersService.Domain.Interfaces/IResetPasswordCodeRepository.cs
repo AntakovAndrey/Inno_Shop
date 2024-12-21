2f3e7ef48fa7d7afa0fa4e639f96c0a366e2f4fb
AntakovAndrey <andrey.antakov05@gmail.com>
2024-12-21 23:11:45 +0300

    IResetPasswordCodeRepository added

---
+++ Services/UsersService/Domain/UsersService.Domain.Interfaces/IResetPasswordCodeRepository.cs
@@ -0,0 +1,13 @@
+﻿using UsersService.Domain.Core;
+
+namespace UsersService.Domain.Interfaces;
+
+public interface IResetPasswordCodeRepository
+{
+    ResetPasswordCode? GetResetPasswordCode(int id);
+    ResetPasswordCode? GetResetPasswordCode(User user);
+    void Create(ResetPasswordCode resetPasswordCode);
+    void Update(ResetPasswordCode resetPasswordCode);
+    void Delete(int id);
+    void Save();
+}
